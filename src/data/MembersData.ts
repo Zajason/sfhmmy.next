6d9fdd9c354bd5c9d0a0f0e4cf09e8947d235084
Iasonas Zakynthinos <zak@fc9eff88-2436-4219-ab87-b815b6be4791.fritz.box>
2025-04-03 01:24:11 +0300

    oc updates

--- src/data/MembersData.ts
+++ src/data/MembersData.ts
@@ -1,84 +1,338 @@
 export const membersData = [
-    {
-      name: "Ιασονας Ζακυνθινος",
-      image: "/images/members/iasonas.png",
-      role: "Front End Developer"
-    },
-    {
-      name: "Στεφανος Φραγκουλης",
-      image: "/images/members/stefanos.png",
-      role: "Back End Developer"
-    },
-    {
-      name: "Χρυσόστομος Κουμίδης",
-      image: "/images/members/chryskoum.jpg",
-      role: "Front End Developer"
-    },
-    {
-      name: "Eleni Papadopoulos",
-      image: "/images/members/flylo.png",
-      role: "Marketing Director"
-    },
-    {
-      name: "Dimitris Konstantinou",
-      image: "/images/members/oldman-modified.png",
-      role: "Product Manager"
-    },
-    {
-      name: "Maria Tsoukalas",
-      image: "/images/members/woman-modified.png",
-      role: "UX/UI Designer"
-    },
-    {
-      name: "Fly Lo",
-      image: "/images/members/flylo.png",
-      role: "CEO"
-    },
-    {
-      name: "Mad Clip",
-      image: "/images/members/oldman-modified.png",
-      role: "CTO"
-    },
-    {
-      name: "Bob Johnson",
-      image: "/images/members/woman-modified.png",
-      role: "CFO"
-    },
-    {
-      name: "Eleni Papadopoulos",
-      image: "/images/members/flylo.png",
-      role: "Marketing Director"
-    },
-    {
-      name: "Dimitris Konstantinou",
-      image: "/images/members/oldman-modified.png",
-      role: "Product Manager"
-    },
-    {
-      name: "Maria Tsoukalas",
-      image: "/images/members/woman-modified.png",
-      role: "UX/UI Designer"
-    },
-    {
-      name: "Fly Lo",
-      image: "/images/members/flylo.png",
-      role: "CEO"
-    },
-    {
-      name: "Mad Clip",
-      image: "/images/members/oldman-modified.png",
-      role: "CTO"
-    },
-    {
-      name: "Bob Johnson",
-      image: "/images/members/woman-modified.png",
-      role: "CFO"
-    },
-    {
-      name: "Eleni Papadopoulos",
-      image: "/images/members/flylo.png",
-      role: "Marketing Director"
-    },
-
-  ];
-  
+  {
+    "name": "Lena Simitsaki",
+    "image": "/images/members/IMG_2018.jpg",
+    "role": "Head Organizer",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria Papakostidi",
+    "image": "/images/members/IMG_1911.jpg",
+    "role": "Logistics Coordinator",
+    "linkedin": ""
+  },
+  {
+    "name": "Despina Togkelidou",
+    "image": "/images/members/IMG_1982.jpg",
+    "role": "Logistics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Vicky Tsintsfou",
+    "image": "/images/members/IMG_1950.jpg",
+    "role": "Logistics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Natalia Anastasia Kousta",
+    "image": "/images/members/IMG_1970.jpg",
+    "role": "Logistics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Eleftheria Roussou",
+    "image": "/images/members/IMG_1990.jpg",
+    "role": "Logistics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Chryssana Georgiadou",
+    "image": "/images/members/IMG_1934.jpg",
+    "role": "Logistics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Aggeliki Tavridou",
+    "image": "/images/members/IMG_1722.jpg",
+    "role": "Fundraising Coordinator",
+    "linkedin": ""
+  },
+  {
+    "name": "Angie Sylai",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Fundraising Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Altzi Tsanko",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Fundraising Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Leonidas Giasanakopoulos",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Fundraising Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria Kara",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Fundraising Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Michalis Chytiroglou",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Fundraising Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Giannis Lazaridis",
+    "image": "/images/members/IMG_2032.jpg",
+    "role": "Academics Coordinator",
+    "linkedin": ""
+  },
+  {
+    "name": "Giorgos Vavouras",
+    "image": "/images/members/IMG_2081.jpg",
+    "role": "Academics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Nikolaos Nikolaidis",
+    "image": "/images/members/IMG_2065.jpg",
+    "role": "Academics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Anestis Sotiropoulos",
+    "image": "/images/members/IMG_2068.jpg",
+    "role": "Academics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "George Anastasiadis",
+    "image": "/images/members/IMG_2039.jpg",
+    "role": "Academics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Milena Leoudi",
+    "image": "/images/members/IMG_2239.JPG",
+    "role": "Academics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Konstantina Gkavanozi",
+    "image": "/images/members/IMG_2050.jpg",
+    "role": "Academics Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Thomas Achilleas Billas",
+    "image": "/images/members/IMG_1580.jpg",
+    "role": "Activities Coordinator",
+    "linkedin": ""
+  },
+  {
+    "name": "Iordanis Chatzikonstantinou",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Activities Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Elena Lazaridou",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Activities Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Haritina Agrafioti",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Activities Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria Sotiria Kostomanolaki",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Activities Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Lefteris Balogiannis",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Activities Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Aimilia Xakoustou",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Activities Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Vasiliki Tserentzoulia",
+    "image": "/images/members/IMG_2162.jpg",
+    "role": "Graphic Design Coordinator",
+    "linkedin": ""
+  },
+  {
+    "name": "Orfeas Siozos",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Graphic Design Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Dimitris Antonopoulos",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Graphic Design Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria Aitsidou",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Graphic Design Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Eleftheria Panagiotidou",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Graphic Design Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria Doulgeridou",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Graphic Design Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Xristina Vadola",
+    "image": "/images/members/view?usp=drive_link",
+    "role": "Graphic Design Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Olga Maria Tsenekidou",
+    "image": "/images/members/IMG_1246.jpg",
+    "role": "Public Relations Coordinator",
+    "linkedin": ""
+  },
+  {
+    "name": "Stavroula Melliou",
+    "image": "/images/members/IMG_1068.jpg",
+    "role": "Public Relations Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria Karanikola",
+    "image": "/images/members/IMG_1103.jpg",
+    "role": "Public Relations Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Eirini Kouzoglou",
+    "image": "/images/members/IMG_1143.jpg",
+    "role": "Public Relations Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Anastasia Tzika",
+    "image": "/images/members/IMG_0992 (2nd).jpg",
+    "role": "Public Relations Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria-Eirini Stylianaki",
+    "image": "/images/members/IMG_1829.jpg",
+    "role": "Public Relations Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Stefanos Fragkoulis",
+    "image": "/images/members/IMG_1349.jpg",
+    "role": "Information Technology Coordinator",
+    "linkedin": ""
+  },
+  {
+    "name": "Thanos Efthymiou",
+    "image": "/images/members/IMG_1290.jpg",
+    "role": "Information Technology Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Chrysostomos Koumides",
+    "image": "/images/members/IMG_1368.jpg",
+    "role": "Information Technology Membe",
+    "linkedin": ""
+  },
+  {
+    "name": "Maria Mamougiorgi",
+    "image": "/images/members/IMG_1416.jpg",
+    "role": "Information Technology Member",
+    "linkedin": ""
+  },
+  {
+    "name": "Iason Daniil Zakynthinos",
+    "image": "/images/members/IMG_3410.png",
+    "role": "Information Technology Member",
+    "linkedin": "https://www.linkedin.com/in/iason-daniil-zakynthinos-b1a172194/"
+  },
+  {
+    "name": "Katerina Papoutsoglou",
+    "image": "/images/members/image0.jpeg",
+    "role": "Ambassador Volos",
+    "linkedin": "https://www.linkedin.com/in/katerina-papoutsoglou-5a27a7251/"
+  },
+  {
+    "name": "Dimitris Voitsidis",
+    "image": "/images/members/sfhmmy.jpg",
+    "role": "Ambassador Volos",
+    "linkedin": "https://www.linkedin.com/in/dimitris-voitsidis/"
+  },
+  {
+    "name": "Maria - Alexia Georgiadou",
+    "image": "/images/members/IMG_1553 (1).jpg",
+    "role": "Ambassador Kozani",
+    "linkedin": "https://www.linkedin.com/in/maria-alexia-georgiadou-0aa1b32a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+  },
+  {
+    "name": "Konstantinos Papathanasiou",
+    "image": "/images/members/0F6BC004-9C74-4EA4-BFFB-67B7CE77D482 (1).jpeg",
+    "role": "Ambassador Kozani",
+    "linkedin": ""
+  },
+  {
+    "name": "Kassiani Karakosta",
+    "image": "/images/members/1.png",
+    "role": "Ambassador Patras",
+    "linkedin": "www.linkedin.com/in/kassiani-karakosta"
+  },
+  {
+    "name": "Nikoletta Maniatakou",
+    "image": "/images/members/Nikol Maniatakou.jpg",
+    "role": "Ambassador Patras",
+    "linkedin": ""
+  },
+  {
+    "name": "Thomas Lagkalis",
+    "image": "/images/members/IMG_20250226_152321.jpg",
+    "role": "Ambassador Chania",
+    "linkedin": ""
+  },
+  {
+    "name": "Konstantinos Pisimisis",
+    "image": "/images/members/IMG_20250304_142613 (1).jpg",
+    "role": "Ambassador Chania",
+    "linkedin": "https://www.linkedin.com/in/kpisimisis-softwarecraft/"
+  },
+  {
+    "name": "Christos Stampoulidis",
+    "image": "/images/members/Christos Stampoulidis.jpg",
+    "role": "Ambassador Xanthi",
+    "linkedin": "https://www.linkedin.com/in/christos-stampoulidis-a6a30022b/"
+  },
+  {
+    "name": "Vasiliki Neila",
+    "image": "/images/members/\u0392\u03b1\u03c3\u03b9\u03bb\u03b9\u03ba\u03ae \u039d\u03b5\u03af\u03bb\u03b1.jpg",
+    "role": "Ambassador Athens",
+    "linkedin": "www.linkedin.com/in/vasilikineila"
+  },
+  {
+    "name": "Stavroula Ziarra",
+    "image": "/images/members/20250228_202814.heic.jpg",
+    "role": "Ambassador Lamia",
+    "linkedin": ""
+  }
+];
--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -1,164 +1,188 @@
-// import React, { useState } from "react";
-// import {
-//   motion,
-//   useTransform,
-//   AnimatePresence,
-//   useMotionValue,
-//   useSpring,
-// } from "framer-motion";
-// import { membersData } from "../data/MembersData"; // Adjust path if necessary
-// import { useTheme } from "../utils/ThemeContext"; // Ensure path to ThemeContext is correct
-// import Image from "next/image"; // Import Next.js Image for optimized images
+import React, { useState } from "react";
+import {
+  motion,
+  useTransform,
+  AnimatePresence,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
+import { membersData } from "../data/MembersData";
+import { useTheme } from "../utils/ThemeContext";
+import Image from "next/image";
+import Link from "next/link";
 
-// const Members = () => {
-//   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-//   const springConfig = { stiffness: 100, damping: 5 };
-//   const x = useMotionValue(0);
+const Members = () => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const springConfig = { stiffness: 100, damping: 5 };
+  const x = useMotionValue(0);
 
-//   const { theme } = useTheme();
+  const { theme } = useTheme();
 
-//   // Set colors based on the theme
-//   const backgroundColor = theme === "dark" ? "bg-black" : "bg-white";
-//   const textColor = theme === "dark" ? "text-white" : "text-blue-900";
-//   const cardBorderColor = theme === "dark" ? "border-white" : "border-gray-800";
-//   const tooltipBackgroundColor = theme === "dark" ? "bg-black" : "bg-gray-200";
-//   const tooltipTextColor = theme === "dark" ? "text-white" : "text-blue-900";
+  const backgroundColor = theme === "dark" ? "bg-black" : "bg-white";
+  const textColor = theme === "dark" ? "text-white" : "text-blue-900";
+  const cardBorderColor = theme === "dark" ? "border-white" : "border-gray-800";
+  const tooltipBackgroundColor = theme === "dark" ? "bg-black" : "bg-gray-200";
+  const tooltipTextColor = theme === "dark" ? "text-white" : "text-blue-900";
 
-//   // Rotate the tooltip
-//   const rotate = useSpring(
-//     useTransform(x, [-100, 100], [-45, 45]),
-//     springConfig
-//   );
+  const rotate = useSpring(
+    useTransform(x, [-100, 100], [-45, 45]),
+    springConfig
+  );
 
-//   // Translate the tooltip
-//   const translateX = useSpring(
-//     useTransform(x, [-100, 100], [-50, 50]),
-//     springConfig
-//   );
+  const translateX = useSpring(
+    useTransform(x, [-100, 100], [-50, 50]),
+    springConfig
+  );
 
-//   const handleMouseMove = (event: any) => {
-//     const halfWidth = event.target.offsetWidth / 2;
-//     x.set(event.nativeEvent.offsetX - halfWidth);
-//   };
+  const handleMouseMove = (event: any) => {
+    const halfWidth = event.target.offsetWidth / 2;
+    x.set(event.nativeEvent.offsetX - halfWidth);
+  };
 
-//   // Animation variants
-//   const fadeInUp = {
-//     hidden: { opacity: 0, y: 20 },
-//     visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
-//   };
+  const fadeInUp = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  };
 
-//   const staggerContainer = {
-//     hidden: { opacity: 0 },
-//     visible: {
-//       opacity: 1,
-//       transition: { staggerChildren: 0.2 },
-//     },
-//   };
+  const staggerContainer = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.2 },
+    },
+  };
 
-//   const items = membersData.map((member, index) => ({
-//     id: index,
-//     name: member.name,
-//     designation: member.role,
-//     image: member.image,
-//   }));
+  const groupedTeams: Record<string, any[]> = {};
+  membersData.forEach((member, index) => {
+    const role = member.role;
+    let team = "Other";
+    if (role.includes("Head Organizer")) team = "1_Head Organizer";
+    else if (role.includes("Information Technology")) team = "2_IT Team";
+    else if (role.includes("Fundraising")) team = "3_Fundraising Team";
+    else if (role.includes("Logistics")) team = "4_Logistics Team";
+    else if (role.includes("Academics")) team = "5_Academics Team";
+    else if (role.includes("Activities")) team = "6_Activities Team";
+    else if (role.includes("Graphic Design")) team = "7_Graphic Design Team";
+    else if (role.includes("Public Relations")) team = "8_Public Relations Team";
+    else if (role.includes("Ambassador")) team = "9_Ambassadors";
 
-//   return (
-//     <div className={`min-h-screen ${backgroundColor}`}>
-//       <motion.div
-//         className={`flex flex-col justify-center items-center min-h-screen ${backgroundColor} pt-20`}
-//         initial="hidden"
-//         animate="visible"
-//         variants={staggerContainer}
-//       >
-//         <motion.h1
-//           variants={fadeInUp}
-//           className={`${textColor} text-4xl font-bold mb-8`}
-//         >
-//           Meet Our Members
-//         </motion.h1>
+    if (!groupedTeams[team]) groupedTeams[team] = [];
+    groupedTeams[team].push({ ...member, id: index });
+  });
 
-//         {/* Grid for Members */}
-//         <motion.div
-//           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 p-8"
-//           variants={staggerContainer}
-//         >
-//           {items.map((item) => (
-//             <motion.div
-//               key={item.id}
-//               className="relative group"
-//               variants={fadeInUp}
-//               onMouseEnter={() => setHoveredIndex(item.id)}
-//               onMouseLeave={() => setHoveredIndex(null)}
-//             >
-//               <AnimatePresence>
-//                 {hoveredIndex === item.id && (
-//                   <motion.div
-//                     initial={{ opacity: 0, y: 20, scale: 0.6 }}
-//                     animate={{
-//                       opacity: 1,
-//                       y: 0,
-//                       scale: 1,
-//                       transition: {
-//                         type: "spring",
-//                         stiffness: 260,
-//                         damping: 10,
-//                       },
-//                     }}
-//                     exit={{ opacity: 0, y: 20, scale: 0.6 }}
-//                     style={{
-//                       translateX: translateX,
-//                       rotate: rotate,
-//                       whiteSpace: "nowrap",
-//                     }}
-//                     className={`absolute -top-16 left-1/2 transform -translate-x-1/2 flex text-xs flex-col items-center justify-center rounded-md ${tooltipBackgroundColor} z-50 shadow-xl px-4 py-2`}
-//                   >
-//                     <div
-//                       className={`font-bold ${tooltipTextColor} relative z-30 text-base`}
-//                     >
-//                       {item.name}
-//                     </div>
-//                     <div className={`${tooltipTextColor} text-xs`}>
-//                       {item.designation}
-//                     </div>
-//                   </motion.div>
-//                 )}
-//               </AnimatePresence>
+  // Sort IT team to place Iasonas directly after the coordinator
+  if (groupedTeams["2_IT Team"]) {
+    const iasonasIndex = groupedTeams["2_IT Team"].findIndex(m => m.name.toLowerCase().includes("zakynthinos"));
+    const coordinatorIndex = groupedTeams["2_IT Team"].findIndex(m => m.role.includes("Coordinator"));
+    if (iasonasIndex !== -1 && coordinatorIndex !== -1 && iasonasIndex !== coordinatorIndex + 1) {
+      const [iasonas] = groupedTeams["2_IT Team"].splice(iasonasIndex, 1);
+      groupedTeams["2_IT Team"].splice(coordinatorIndex + 1, 0, iasonas);
+    }
+  }
 
-//               {/* Profile Image */}
-//               <motion.div
-//                 className="relative h-40 w-40 overflow-hidden rounded-full border-2 group-hover:scale-105 group-hover:z-30 transition duration-500"
-//                 style={{
-//                   aspectRatio: "1 / 1",
-//                   borderColor: cardBorderColor,
-//                 }}
-//                 whileHover={{ scale: 1.1 }}
-//               >
-//                 <Image
-//                   onMouseMove={handleMouseMove}
-//                   src={`${item.image}`}
-//                   alt={item.name}
-//                   layout="fill"
-//                   objectFit="cover"
-//                   objectPosition="center"
-//                 />
-//               </motion.div>
-//             </motion.div>
-//           ))}
-//         </motion.div>
-//       </motion.div>
-//     </div>
-//   );
-// };
+  const sortedGroups = Object.entries(groupedTeams).sort(([a], [b]) => a.localeCompare(b));
 
-// export default Members;
-import React from "react";
-import ComingSoon from "../components/ComingSoon";
-
-const Members: React.FC = () => {
   return (
-    <>
-      <ComingSoon />
-    </>
+    <div className={`min-h-screen ${backgroundColor}`}>
+      <motion.div
+        className={`flex flex-col justify-center items-center min-h-screen ${backgroundColor} pt-20`}
+        initial="hidden"
+        animate="visible"
+        variants={staggerContainer}
+      >
+        <motion.h1
+          variants={fadeInUp}
+          className={`${textColor} text-4xl font-bold mb-8`}
+        >
+          Meet Our Members
+        </motion.h1>
+
+        {sortedGroups.map(([team, items]) => (
+          <div key={team} className="w-full mb-16">
+            <h2 className={`text-2xl font-semibold mb-6 text-center ${textColor}`}>{team.replace(/^\d+_/, '')}</h2>
+            <motion.div
+              className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4 justify-center px-4"
+              variants={staggerContainer}
+            >
+              {items.map((item) => (
+                <motion.div
+                  key={item.id}
+                  className="relative group"
+                  variants={fadeInUp}
+                  onMouseEnter={() => setHoveredIndex(item.id)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                >
+                  <AnimatePresence>
+                    {hoveredIndex === item.id && (
+                      <motion.div
+                        initial={{ opacity: 0, y: 20, scale: 0.6 }}
+                        animate={{
+                          opacity: 1,
+                          y: 0,
+                          scale: 1,
+                          transition: {
+                            type: "spring",
+                            stiffness: 260,
+                            damping: 10,
+                          },
+                        }}
+                        exit={{ opacity: 0, y: 20, scale: 0.6 }}
+                        style={{
+                          translateX: translateX,
+                          rotate: rotate,
+                          whiteSpace: "nowrap",
+                        }}
+                        className={`absolute -top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center rounded-md ${tooltipBackgroundColor} z-50 shadow-xl px-4 py-2`}
+                      >
+                        <div className={`font-bold ${tooltipTextColor} text-base`}>
+                          {item.name}
+                        </div>
+                        <div className={`${tooltipTextColor} text-xs mb-1`}>
+                          {item.role}
+                        </div>
+                        {item.linkedin && (
+                          <a
+                            href={item.linkedin.startsWith("http") ? item.linkedin : `https://${item.linkedin}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mt-1"
+                          >
+                            <Image
+                              src="/images/socials/linkedin.png"
+                              alt="LinkedIn"
+                              width={16}
+                              height={16}
+                            />
+                          </a>
+                        )}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+
+                  <motion.div
+                    className="relative h-52 w-52 overflow-hidden rounded-full border-2 group-hover:scale-105 group-hover:z-30 transition duration-500 mx-auto"
+                    style={{
+                      aspectRatio: "1 / 1",
+                      borderColor: cardBorderColor,
+                    }}
+                    whileHover={{ scale: 1.1 }}
+                  >
+                    <Image
+                      onMouseMove={handleMouseMove}
+                      src={`${item.image}`}
+                      alt={item.name}
+                      layout="fill"
+                      objectFit="cover"
+                      objectPosition="center"
+                    />
+                  </motion.div>
+                </motion.div>
+              ))}
+            </motion.div>
+          </div>
+        ))}
+      </motion.div>
+    </div>
   );
 };
 
